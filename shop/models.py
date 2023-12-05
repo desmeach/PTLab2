ef2cc7354cbe187c79617f3196d258adb9ba58f0
Vladislav <desmeach@gmail.com>
2023-12-06 00:45:58 +0300

    Discount get methods moved to models.py

--- shop/models.py
+++ shop/models.py
@@ -8,6 +8,11 @@ class Product(models.Model):
     start_quantity = models.PositiveIntegerField(default=0)
     quantity = models.PositiveIntegerField(default=0)
 
+    def get_discount_price(self, discount):
+        if self.price <= discount.start_quantity // 2 < self.quantity:
+            return self.price * (1 + discount.discount_percentage)
+        return self.price
+
 
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -21,3 +26,14 @@ class Discount(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     start_quantity = models.PositiveIntegerField(default=0)
     discount_percentage = models.FloatField(default=0.2)
+
+    @staticmethod
+    def get_by_product(product):
+        if Discount.objects.filter(product=product.id).exists():
+            discount = Discount.objects.get(product=product)
+        else:
+            discount = Discount.objects.create(
+                product=product,
+                start_quantity=product.quantity
+            )
+        return discount
--- shop/views.py
+++ shop/views.py
@@ -20,24 +20,10 @@ class PurchaseCreate(CreateView):
         self.object = form.save()
         product = Product.objects.get(id=self.object.product.id)
         new_quantity = product.quantity - self.object.quantity
-        new_price = product.price
         if new_quantity < 0:
             return HttpResponse(f'К сожалению, выбранного количества товара нет')
-        discount_quantity = product.quantity
-        discount_percentage = 0.2
-        if Discount.objects.filter(product=self.object.product.id).exists():
-            discount = Discount.objects.get(product=self.object.product)
-            discount_quantity = discount.start_quantity
-            discount_percentage = discount.discount_percentage
-        else:
-            Discount.objects.create(
-                product=self.object.product,
-                start_quantity=product.quantity,
-                discount_percentage=discount_percentage
-            )
-        if new_quantity <= discount_quantity // 2 < product.quantity:
-            new_price = product.price * (1 + discount_percentage)
-        product.price = new_price
+        discount = Discount.get_by_product(product)
+        product.price = product.get_discount_price(discount)
         product.quantity = new_quantity
         product.save()
         return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
