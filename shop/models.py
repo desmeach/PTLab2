ec637e5eac451c17ca65f7c5e01d14b572809ae9
Vladislav <desmeach@gmail.com>
2023-12-05 22:51:21 +0300

    Added quantity to products.  20% discounts for 50% product quantity.

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=0)
+
 
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     person = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
+    quantity = models.PositiveIntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
+
+
+class Discount(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    start_quantity = models.PositiveIntegerField(default=0)
--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 
-from .models import Product, Purchase
+from .models import Product, Purchase, Discount
+
 
 # Create your views here.
 def index(request):
@@ -13,9 +14,27 @@ def index(request):
 
 class PurchaseCreate(CreateView):
     model = Purchase
-    fields = ['product', 'person', 'address']
+    fields = ['product', 'person', 'quantity', 'address']
 
     def form_valid(self, form):
         self.object = form.save()
+        product = Product.objects.get(id=self.object.product.id)
+        new_quantity = product.quantity - self.object.quantity
+        new_price = product.price
+        if new_quantity < 0:
+            return HttpResponse(f'К сожалению, выбранного количества товара нет')
+        discount_quantity = product.quantity
+        if Discount.objects.filter(product=self.object.product.id).exists():
+            discount = Discount.objects.get(product=self.object.product)
+            discount_quantity = discount.start_quantity
+        if new_quantity <= discount_quantity // 2:
+            Discount.objects.create(
+                product=self.object.product,
+                start_quantity=discount_quantity // 2
+            )
+            new_price = product.price * 1.2
+        product.price = new_price
+        product.quantity = new_quantity
+        product.save()
         return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
 
